a90843013df07ced6c49e143929a19cc6c4a9f54
owel <rwlllaguitan@yahoo.com>
2021-07-05 17:07:16 +0800

    added lesson 16 selenium grid demo

--- src/test/java/Pluralsight/WebDriverDemoServer.java
+++ src/test/java/Pluralsight/WebDriverDemoServer.java
@@ -18,7 +18,7 @@ public class WebDriverDemoServer {
 
     public static void main(String[] args) throws MalformedURLException {
 
-        System.setProperty("webdriver.chrome.driver","D://owel//Documents//1_Owel Docs//Training//Driver//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","D://owel//Documents//Training//Driver//chromedriver.exe");
 //        WebDriver webDriver = new ChromeDriver();
 
         WebDriver webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());
