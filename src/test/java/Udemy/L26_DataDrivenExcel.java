8f2ed1bccc05c09489e62efc2516117d5e113853
owel <rwlllaguitan@yahoo.com>
2021-08-03 20:29:51 +0800

    access rows and columns in the excel

--- src/test/java/Udemy/L26_DataDrivenExcel.java
+++ src/test/java/Udemy/L26_DataDrivenExcel.java
@@ -1,10 +1,13 @@
 package Udemy;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 /*
 Basic template para makapag traverse sa sheet
@@ -13,19 +16,34 @@ fis - object that gives permission to read the file. Ito lang yung tinatanggap n
 public class L26_DataDrivenExcel {
 
     public static void main(String[] args) throws IOException {
+
+        //declare fis and workbook
         FileInputStream fis = new FileInputStream("C:\\Users\\User\\Documents\\Driver\\CardDetails.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
 
+        //kunin mo yung number of sheets para sa for loop
         int sheets = workbook.getNumberOfSheets();
 
+        //pangtraverse lan ito sa lahat ng sheet
         for(int i = 0; i < sheets; i++){
 
-            //after mavalidate yung correct sheet name gamit yung if
-            //getSheetName - kailangan nya ng int para magreturn ng string at para makapagcompare tayo
-            //idedeclare natin yung sheet para makapagtraverse pa
-
+            //if condition to check yung sheetName tapos idedeclare as sheet
             if(workbook.getSheetName(i).equalsIgnoreCase("Sheet1")){
                 XSSFSheet sheet = workbook.getSheetAt(i);
+
+                //access yung first row
+                Iterator<Row> rows = sheet.iterator();
+                Row firstRow = rows.next();
+
+                //access yung columns
+                Iterator<Cell> cells = firstRow.cellIterator();
+                while(cells.hasNext()){
+
+                    Cell value = cells.next();
+                    if(value.getStringCellValue().equalsIgnoreCase("CardNumber")){
+                        System.out.println("true");
+                    }
+                }
 
             }
         }
