9188d00beb8781d2faa40ef5451f7f30c86bc328
owel <rwlllaguitan@yahoo.com>
2021-08-06 09:43:16 +0800

    rewrite logic for reading excel

---
+++ src/test/java/Udemy/L26_DataDrivenExcel2.java
@@ -0,0 +1,64 @@
+package Udemy;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class L26_DataDrivenExcel2 {
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis = new FileInputStream("C:\\Users\\User\\Documents\\Driver\\CardDetails.xlsx");
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+
+        int numSheet = workbook.getNumberOfSheets();
+
+        for(int i = 0 ; i < numSheet ; i++ ){
+            if(workbook.getSheetName(i).equalsIgnoreCase("Sheet2")){
+
+                XSSFSheet correctSheet = workbook.getSheetAt(i);
+
+                Iterator<Row> rowIterator = correctSheet.iterator();
+                Row firstRow = rowIterator.next();
+
+                Iterator<Cell> cells = firstRow.cellIterator();
+
+                while(cells.hasNext()){
+
+                    Cell headerValue = cells.next();
+                    if(headerValue.getStringCellValue().equalsIgnoreCase("Test Case")){
+                        System.out.println(headerValue);
+                        System.out.println(headerValue.getColumnIndex());
+                        int correctColumn = headerValue.getColumnIndex();
+
+                        while(rowIterator.hasNext()){
+                            Row rowSearch = rowIterator.next();
+                            if(rowSearch.getCell(correctColumn).getStringCellValue().equalsIgnoreCase("Login")){
+                                Iterator<Cell> cellGrab = rowSearch.cellIterator();
+
+                                while (cellGrab.hasNext()){
+                                    System.out.println(cellGrab.next());
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+
+
+
+
+            }
+
+        }
+
+
+    }
+
+}
